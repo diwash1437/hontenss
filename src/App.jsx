37cba5c0d577d895a49e9aafc02da31ce22fba0e
diwash1437 <diwashlimbu47@gmail.com>
2026-03-10 16:34:08 +0900

    nabata problem

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
  <Routes>
   <Route path="/" element={<Restaurant/>}/>
-  <Route path="/nabata" element={<NabataColtd153/>}/>
+  {/* <Route path="/nabata" element={<NabataColtd153/>}/> */}
   {/* <Route path='/tsCorporation' element={<TsCorporation/>}/> */}
   <Route path='/fujimatsu' element={<FujimatsuColtd/>}/>
   {/* <Route path='/try' element={<Trycheckitem/>}/> */}
